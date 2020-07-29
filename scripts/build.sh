3bfaefd9d5007532d792aecd59f241c1f1e02f06
unknown <o.d.kopot@gmail.com>
2020-07-29 10:17:51 +0300

    Fixed error in files' names

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-docker build -f .docker/api.DockerFile -t api .
-docker build -f .docker/client.DockerFile -t client .
+docker build -f .docker/api.Dockerfile -t api .
+docker build -f .docker/client.Dockerfile -t client .
 
 docker system prune -f
