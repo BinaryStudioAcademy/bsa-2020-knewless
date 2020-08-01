348469fcc2180411c1bd069c76477a2dedeba9a4
unknown <o.d.kopot@gmail.com>
2020-08-01 15:00:33 +0300

    Changed ssh command, commented build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-docker build -f .docker/api.Dockerfile -t api .
-docker build -f .docker/client.Dockerfile -t client .
+#docker build -f .docker/api.Dockerfile -t api .
+#docker build -f .docker/client.Dockerfile -t client .
 
-docker login -u $docker_user -p $docker_pass
+#docker login -u $docker_user -p $docker_pass
 
-docker tag api:latest bsa2020knewless/dockerhub:api
-docker tag client:latest bsa2020knewless/dockerhub:client
+#docker tag api:latest bsa2020knewless/dockerhub:api
+#docker tag client:latest bsa2020knewless/dockerhub:client
 
-docker push bsa2020knewless/dockerhub:api
-docker push bsa2020knewless/dockerhub:client
+#docker push bsa2020knewless/dockerhub:api
+#docker push bsa2020knewless/dockerhub:client
