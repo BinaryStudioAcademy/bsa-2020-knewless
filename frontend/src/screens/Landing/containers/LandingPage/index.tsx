0d1d9c37356d5dca65e7da6df88d2a0832953105
Quipex <rudas72@gmail.com>
2020-08-07 21:02:38 +0300

    Fixed cards layout when less than 3. Fixed cropped lines at path cards added ellipsis on text overflow more than 2 lines.

--- frontend/src/screens/Landing/containers/LandingPage/index.tsx
+++ frontend/src/screens/Landing/containers/LandingPage/index.tsx
@@ -135,7 +135,7 @@ export const LandingPage: React.FunctionComponent<ILandingProps> = ({
       </div>
       <div className={styles.footer__layer}>
         <footer className={styles.wide_container}>
-          <span>Copyright © 2018 Grada. All rights reserved.</span>
+          <span>Copyright © 2020 BSA. All rights reserved.</span>
           <a href="/" className={styles.footer__align_right}>Terms & Conditions</a>
           <a href="/">Privacy Policy</a>
         </footer>
