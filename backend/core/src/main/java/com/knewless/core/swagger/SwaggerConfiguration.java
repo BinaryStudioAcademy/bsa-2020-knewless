d260f2bfc4c0d58e365ef556ebd2e7c70d18addf
Artur <34406335+ArturTrainee@users.noreply.github.com>
2020-08-12 11:08:39 +0300

    Feature/54 swagger (#43)
    * Added swagger configuration
    
    * Returned app root permission
    
    * Added authorization header support to swagger UI

---
+++ backend/core/src/main/java/com/knewless/core/swagger/SwaggerConfiguration.java
@@ -0,0 +1,81 @@
+package com.knewless.core.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+
+import java.util.Collections;
+import java.util.List;
+
+/*
+    Swagger UI Link: http://localhost:5000/swagger-ui/index.html#/
+ */
+@Configuration
+public class SwaggerConfiguration {
+
+    public static final String[] SWAGGER_PATHS_WHITELIST = {
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/v2/api-docs"
+    };
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
+
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final var authorizationScopes = new AuthorizationScope[]{
+                new AuthorizationScope("global", "accessEverything")
+        };
+        return Collections.singletonList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+
+}
