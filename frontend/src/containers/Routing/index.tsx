e45393a138fa18b4ac750124a1fc6fbd6ec3af92
Artur <34406335+ArturTrainee@users.noreply.github.com>
2020-08-13 12:38:36 +0300

    Role based main page routing (#53)
    * Added role based main page routing
    
    * Replaced Public route with private route
    
    * Added condition if user role is invalid
    
    * Fixed mock user id & removed role foeld
    
    * Removed user mock

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -8,9 +8,8 @@ import Header from '../../components/Header';
 import AddCourse from '../../screens/AddCourse/containers/AddCoursePage';
 import LoginPage from '../../screens/Authentication/containers/LoginPage';
 import handler from '../../components/OAuth2RedirectHandler/OAuth2RedirectHandler';
-import MainStudentPage from '../../screens/MainPage/containers/MainStudentPage';
 import LecturePage from 'screens/LecturePage/containers/Lectures/index';
-import MainAuthorPage from '../../screens/AuthorMainPage/containers/MainPage';
+import SettingsRoute from 'containers/SettingsRoute';
 import RegisterPage from '../../screens/Authentication/containers/RegisterPage';
 import AuthorPublicPage from '../../screens/AuthorPublicPage/containers/AuthorPublicPage';
 import StudentProfile from 'screens/StudentPage/containers/StudentProfilePage';
@@ -18,24 +17,14 @@ import { ACCESS_TOKEN } from '../../screens/Authentication/constants';
 import { IAppState } from '../../models/AppState';
 import { connect } from 'react-redux';
 import { IUser } from 'containers/AppRouter/models/IUser';
+import MainPageRoute from '../MainPageRoute';
 import WebSocketNotifications from 'containers/WebSocketNotifications';
 import { AddPathPage } from 'screens/AddPath/containers/AddPathPage';
-import SettingsRoute from 'containers/SettingsRoute';
 
 export interface IRoutingProps {
   isLoading: boolean;
   user: IUser;
 }
-
-const mock: IUser = {
-  id: '1',
-  nickname: 'name',
-  avatar: 'https://media1.tenor.com/images/6f4fa5fea73897955d4b0508c47eeca5/tenor.gif?itemid=14645687',
-  role: { name: 'STUDENT' }
-};
-
-const role = Math.round(Math.random());
-console.log(role);
 
 const Routing: React.FunctionComponent<IRoutingProps> = ({ isLoading, user }) => {
   const connectToWebsocket = () => {
@@ -62,7 +51,7 @@ const Routing: React.FunctionComponent<IRoutingProps> = ({ isLoading, user }) =>
           <PublicRoute exact path="/landing" component={LandingPage} />
           <PublicRoute exact path="/add_course" component={AddCourse} />
           <PublicRoute exact path="/login" component={LoginPage} />
-          <PublicRoute exact path="/main" component={mock.role.name === 'STUDENT' ? MainStudentPage : MainAuthorPage} />
+          <MainPageRoute />
           <PublicRoute exact path="/oauth/redirect" component={handler} />
           <PublicRoute exact path="/register" component={RegisterPage} />
           <PublicRoute exact path="/author/:authorId" component={AuthorPublicPage} />
