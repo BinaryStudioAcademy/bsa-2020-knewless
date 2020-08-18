078443d26c5f89783199c09e40a8909fd9096c4f
FHToE <m.oleinik89@gmail.com>
2020-08-18 10:44:45 +0300

    start sender folder added

---
+++ backend/emailsender/src/main/java/com/emailsender/EmailsenderApplication.java
@@ -0,0 +1,13 @@
+package com.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailsenderApplication.class, args);
+	}
+
+}
