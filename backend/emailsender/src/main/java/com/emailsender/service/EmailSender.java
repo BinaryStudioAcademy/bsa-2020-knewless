f4799738142e5a6367983eda46ac24e1ea19e228
FHToE <m.oleinik89@gmail.com>
2020-08-21 10:03:59 +0300

    before pull dev

--- backend/emailsender/src/main/java/com/emailsender/service/EmailSender.java
+++ backend/emailsender/src/main/java/com/emailsender/service/EmailSender.java
@@ -22,15 +22,19 @@ public class EmailSender {
     private String KNEWLESS_EMAIL;
     @Value("${mail.password}")
     private String PASSWORD;
+    @Value("${mail.host}")
+    private String HOST;
+    @Value("${mail.port}")
+    private String PORT;
 
     
     public void send(String email, String body, String subject) {
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.host", HOST);
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", HOST);
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", PORT);
         Session s = Session.getInstance(props, new javax.mail.Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
