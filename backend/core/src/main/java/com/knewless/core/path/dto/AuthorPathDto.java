ea9166cd54257fd13009295d4368d6daa090eb5c
Artur <34406335+ArturTrainee@users.noreply.github.com>
2020-08-29 23:52:38 +0300

    Hotfix/249 author main page path link (#199)
    * Added path id to author paths request
    
    * Added path id to path card on author main page

--- backend/core/src/main/java/com/knewless/core/path/dto/AuthorPathDto.java
+++ backend/core/src/main/java/com/knewless/core/path/dto/AuthorPathDto.java
@@ -3,9 +3,11 @@ package com.knewless.core.path.dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Data
 public class AuthorPathDto {
+    private UUID id;
     private String name;
     private long courses;
     private String logoSrc;
