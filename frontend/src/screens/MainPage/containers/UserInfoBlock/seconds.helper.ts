acecf4b456ac299b2a8e26d29aaf839bbe4eb5f7
Dmitriy_R <30311108+Quipex@users.noreply.github.com>
2020-08-31 16:23:58 +0300

    Changed the way time is calculated (#231)
    Now all hours are displayed (only 24 before)

--- frontend/src/screens/MainPage/containers/UserInfoBlock/seconds.helper.ts
+++ frontend/src/screens/MainPage/containers/UserInfoBlock/seconds.helper.ts
@@ -1,3 +1,9 @@
-export function secondsFormatted(secondsOverall: number) {
-  return new Date(secondsOverall * 1000).toISOString().substr(11, 8);
+export function secondsFormatted(secondsOverall: number): string {
+  const hours = Math.floor(secondsOverall / 3600);
+  const minutes = Math.floor((secondsOverall - (hours * 3600)) / 60);
+  const seconds = secondsOverall - (hours * 3600) - (minutes * 60);
+  const hoursFmt = hours < 10 ? `0${hours}` : hours;
+  const minutesFmt = minutes < 10 ? `0${minutes}` : minutes;
+  const secondsFmt = seconds < 10 ? `0${seconds}` : seconds;
+  return `${hoursFmt}:${minutesFmt}:${secondsFmt}`;
 }
