0df2b2f0366eefe07dab18ededfa69f431f9d504
Dmitriy_R <30311108+Quipex@users.noreply.github.com>
2020-09-05 18:06:23 +0300

    Replaced LocalDate with ZonedDate for the proper message time display (#281)

--- backend/core/src/main/java/com/knewless/core/lecture/lectureComment/dto/LectureCommentDto.java
+++ backend/core/src/main/java/com/knewless/core/lecture/lectureComment/dto/LectureCommentDto.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.knewless.core.user.dto.BriefUserDto;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Data
 public class LectureCommentDto {
 	private UUID id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
-	private LocalDateTime createdAt;
+	private ZonedDateTime createdAt;
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
-	private LocalDateTime updatedAt;
+	private ZonedDateTime updatedAt;
 	private String text;
 	private UUID lectureId;
 	private BriefUserDto user;
