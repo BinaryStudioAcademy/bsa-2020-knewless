4053eb795725fc654bd31d751e19cb6fef193cdf
Dmitriy_R <30311108+Quipex@users.noreply.github.com>
2020-09-09 00:02:56 +0300

    Fix page layout, rebuilt author's card layout (#315)

--- frontend/src/screens/PathPage/components/AuthorCard/index.tsx
+++ frontend/src/screens/PathPage/components/AuthorCard/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import Ellipsis from 'react-ellipsis-pjs';
 import styles from './styles.module.sass';
-import noAvatar from '@images/no_avatar.jpg';
+import Avatar from '@components/avatar/Avatar';
 
 interface IAuthorCardProps {
   name: string;
   biography: string;
   imageSrc: string;
+  followers: number;
 }
 
 const AuthorCard: React.FC<IAuthorCardProps> = ({
   name,
   biography,
-  imageSrc
+  imageSrc,
+  followers
 }) => (
   <div className={styles.card}>
-    <img src={imageSrc || noAvatar} className={styles.author__image} alt="Author" />
-    <div className={styles.name}>
-      {name}
-    </div>
-    <div className={styles.biography}>
-      <Ellipsis text={biography} lines={2} />
+    <Avatar imageSrc={imageSrc} className={styles.author__image} alt="Author" />
+    <div className={styles.info}>
+      <div className={styles.name}>
+        {name}
+      </div>
+      <div className={styles.biography}>
+        <span className={styles.followers}>{followers === 1 ? '1 follower' : `${followers} followers`}</span>
+        {biography && <Ellipsis text={biography} lines={2} />}
+      </div>
     </div>
   </div>
 );
