8b090f09afbe57471166a331b9b99ed05f541d70
Dmitriy_R <30311108+Quipex@users.noreply.github.com>
2020-09-08 18:41:35 +0300

    Extracted common logic, handled updatedAt date equals null (#312)
    * Extracted common logic, handled updatedAt date equals null
    
    * Merged

--- backend/core/src/main/java/com/knewless/core/elasticsearch/model/EsMapper.java
+++ backend/core/src/main/java/com/knewless/core/elasticsearch/model/EsMapper.java
@@ -3,11 +3,13 @@ package com.knewless.core.elasticsearch.model;
 import com.knewless.core.author.model.Author;
 import com.knewless.core.course.courseReaction.model.CourseReaction;
 import com.knewless.core.course.model.Course;
+import com.knewless.core.db.BaseEntity;
 import com.knewless.core.lecture.model.Lecture;
 import com.knewless.core.path.model.Path;
 import com.knewless.core.school.model.School;
 import com.knewless.core.tag.model.Tag;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,91 +20,90 @@ import java.util.stream.Collectors;
  * frontend/src/screens/SearchResultsPage/models/EsModels.ts
  */
 public class EsMapper {
-
-    public static EsEntity esEntityFromAuthorEntity(Author entity) {
-        if (entity == null) {
-            return null;
-        }
-
-        Map<String, Object> metadata = new HashMap<>();
-        metadata.put("avatar", entity.getAvatar());
-        metadata.put("biography", entity.getBiography());
-        metadata.put("company", entity.getCompany());
-        metadata.put("job", entity.getJob());
-
-        return EsEntity.builder()
-                .name(entity.getFullName())
-                .updatedAt(entity.getUpdatedAt().toString())
-                .sourceId(entity.getId())
-                .type(EsDataType.AUTHOR)
-                .metadata(metadata)
-                .build();
-    }
-
-    public static EsEntity esEntityFromCourseEntity(Course course, List<String> tags) {
-        if (course == null) {
-            return null;
-        }
-
-        Map<String, Object> metadata = new HashMap<>();
-        metadata.put("image", course.getImage());
-        metadata.put("author", course.getAuthor().getFullName());
-        metadata.put("authorId", course.getAuthor().getId());
-        metadata.put("date", course.getReleasedDate());
-        metadata.put("level", course.getLevel().toString());
-        metadata.put("total minutes", course.getLectures().stream().mapToInt(Lecture::getDuration).sum());
-        metadata.put("lectures", course.getLectures().size());
-        metadata.put("rating", (int) Math.round(course.getReactions().stream().mapToInt(CourseReaction::getReaction).average().orElse(0)));
-        metadata.put("ratingCount", 0);
-
-        return EsEntity.builder()
-                .name(course.getName())
-                .updatedAt(course.getUpdatedAt().toString())
-                .sourceId(course.getId())
-                .type(EsDataType.COURSE)
-                .tags(tags)
-                .metadata(metadata)
-                .build();
-    }
-
-    public static EsEntity esEntityFromPathEntity(Path path) {
-        if (path == null) {
-            return null;
-        }
-
-        Map<String, Object> metadata = new HashMap<>();
-        var duration = path.getCourses().stream().mapToInt(
-                c -> c.getLectures().stream().mapToInt(Lecture::getDuration).sum()
-        ).sum();
-        metadata.put("total minutes", duration);
-        metadata.put("image", path.getImageTag().getSource());
-        metadata.put("courses", path.getCourses().size());
-        metadata.put("author", path.getAuthor().getFullName());
-
-        return EsEntity.builder()
-                .name(path.getName())
-                .updatedAt(path.getUpdatedAt().toString())
-                .tags(path.getTags().stream().map(Tag::getName).collect(Collectors.toList()))
-                .sourceId(path.getId())
-                .type(EsDataType.PATH)
-                .metadata(metadata)
-                .build();
-    }
-
-    public static EsEntity esEntityFromSchoolEntity(School school, List<Author> authors) {
-        if (school == null) {
-            return null;
-        }
-
-        Map<String, Object> metadata = new HashMap<>();
-        metadata.put("authors number", authors.size());
-
-        return EsEntity.builder()
-                .updatedAt(school.getUpdatedAt().toString())
-                .name(school.getName())
-                .type(EsDataType.SCHOOL)
-                .sourceId(school.getId())
-                .metadata(metadata)
-                .build();
-    }
+	
+	public static EsEntity esEntityFromAuthorEntity(Author author) {
+		if (author == null) {
+			return null;
+		}
+		
+		Map<String, Object> metadata = new HashMap<>();
+		metadata.put("avatar", author.getAvatar());
+        metadata.put("biography", author.getBiography());
+        metadata.put("company", author.getCompany());
+        metadata.put("job", author.getJob());
+		
+		return builderFromBaseEntity(author)
+				.name(author.getFullName())
+				.type(EsDataType.AUTHOR)
+				.metadata(metadata)
+				.build();
+	}
+	
+	public static EsEntity esEntityFromCourseEntity(Course course, List<String> tags) {
+		if (course == null) {
+			return null;
+		}
+		
+		Map<String, Object> metadata = new HashMap<>();
+		metadata.put("image", course.getImage());
+		metadata.put("author", course.getAuthor().getFullName());
+		metadata.put("authorId", course.getAuthor().getId());
+		metadata.put("date", course.getReleasedDate());
+		metadata.put("level", course.getLevel().toString());
+		metadata.put("total minutes", course.getLectures().stream().mapToInt(Lecture::getDuration).sum());metadata.put("lectures", course.getLectures().size());
+		metadata.put("rating", (int) Math.round(course.getReactions().stream().mapToInt(CourseReaction::getReaction).average().orElse(0)));
+		metadata.put("ratingCount", 0);
+		
+		return builderFromBaseEntity(course)
+				.name(course.getName())
+				.type(EsDataType.COURSE)
+				.tags(tags)
+				.metadata(metadata)
+				.build();
+	}
+	
+	public static EsEntity esEntityFromPathEntity(Path path) {
+		if (path == null) {
+			return null;
+		}
+		
+		Map<String, Object> metadata = new HashMap<>();
+		var duration = path.getCourses().stream().mapToInt(
+				c -> c.getLectures().stream().mapToInt(Lecture::getDuration).sum()
+		).sum();
+		metadata.put("total minutes", duration);
+		metadata.put("image", path.getImageTag().getSource());
+		metadata.put("courses", path.getCourses().size());
+		metadata.put("author", path.getAuthor().getFullName());
+		
+		return builderFromBaseEntity(path)
+				.name(path.getName())
+				.tags(path.getTags().stream().map(Tag::getName).collect(Collectors.toList()))
+				.type(EsDataType.PATH)
+				.metadata(metadata)
+				.build();
+	}
+	
+	public static EsEntity esEntityFromSchoolEntity(School school, List<Author> authors) {
+		if (school == null) {
+			return null;
+		}
+		
+		Map<String, Object> metadata = new HashMap<>();
+		metadata.put("authors number", authors.size());
+		
+		return builderFromBaseEntity(school)
+				.name(school.getName())
+				.type(EsDataType.SCHOOL)
+				.metadata(metadata)
+				.build();
+	}
+	
+	private static EsEntity.EsEntityBuilder builderFromBaseEntity(BaseEntity entity) {
+		Date updatedAt = entity.getUpdatedAt() == null ? new Date() : entity.getUpdatedAt();
+		return EsEntity.builder()
+				.updatedAt(updatedAt.toString())
+				.type(EsDataType.SCHOOL)
+				.sourceId(entity.getId());
+	}
 }
