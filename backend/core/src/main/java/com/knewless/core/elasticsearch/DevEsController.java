7743c757aee5b29999f2524c2ac6547d029bcf1c
Dmitriy_R <30311108+Quipex@users.noreply.github.com>
2020-09-09 21:38:22 +0300

    Added ability to clear all items at ES. Added messages after successful sync or clear (#324)

--- backend/core/src/main/java/com/knewless/core/elasticsearch/DevEsController.java
+++ backend/core/src/main/java/com/knewless/core/elasticsearch/DevEsController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +25,24 @@ public class DevEsController {
 	
 	
 	@GetMapping("/es/sync")
-	public void synchronise(@RequestHeader(name = "X-CONTROL-TOKEN") String token) {
+	public String synchronise(@RequestHeader(name = "X-CONTROL-TOKEN") String token) {
 		if (!secret.equals(token)) {
 			log.warn("Tried to sync with wrong X-CONTROL-TOKEN {}", token);
-			return;
+			throw new AccessDeniedException("Wrong token");
 		}
 		log.info("Syncing ES with storage...");
-		devEsService.syncWithStorage();
+		return devEsService.syncWithStorage();
+	}
+	
+	@DeleteMapping("/es/clear")
+	public String clear(@RequestHeader(name = "X-CONTROL-TOKEN") String token) {
+		if (!secret.equals(token)) {
+			log.warn("Tried to sync with wrong X-CONTROL-TOKEN {}", token);
+			throw new AccessDeniedException("Wrong token");
+		}
+		log.info("Clearing ES storage...");
+		long items = devEsService.clear();
+		log.info("Cleared {} items", items);
+		return "Deleted " + items + " items";
 	}
 }
