ba23939779c3de70f2338628bfb35774c12f288a
oleg.kopot@igamemedia.com-at-232781373758 <oleg.kopot@igamemedia.com>
2020-09-10 13:49:08 +0300

    Add new tags and professions

---
+++ backend/core/src/main/resources/db/migration/V4__add_tags.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.tags (id, created_at, updated_at,name, source)
+values ('fe2ccb98-a0e8-4882-ba1a-8f1273948e42',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'AI',	'https://i.imgur.com/oYQDlZW.png'),
+('13a4adf3-7749-4445-9a98-5c8e273f52c3',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'Database',	'https://i.imgur.com/w2Vpcms.png'),
+('fa4c0e96-7de1-4018-9e44-269a192b664f',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'Docker',	'https://i.imgur.com/2GJ1bki.png'),
+('84e60093-9f9e-4c75-8eb8-904d8d2b6f42',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'Github',	'https://i.imgur.com/4raLSmv.png'),
+('ef85e116-f6f0-43b1-ba83-80bb053e79ba',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'Graphic',	'https://i.imgur.com/nRWq0gA.png'),
+('0f17e931-bfe0-42c1-a181-535f491fe9f5',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'Linux',	'https://i.imgur.com/2yrDOtc.png'),
+('5fa68519-4074-435a-bc54-93332bad785c',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'MongoDB',	'https://i.imgur.com/zGIvuDV.png'),
+('2f497d24-50d1-4d70-8c2f-1658a5524d79',	'2020-08-05 08:32:07.701',	'2020-08-05 08:32:07.701',	'SQL',	'https://i.imgur.com/Em9yq6C.png')
